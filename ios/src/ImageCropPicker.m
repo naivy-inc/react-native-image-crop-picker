d93eee092d65dac0bb50cfadf54af3a6bf2d3809
woosung <monnalija@naver.com>
2022-06-21 18:30:16 +0900

    Feat: iOS 이미지 여러개 선택시 누른 순서로 반환하는 로직 추가

--- ios/src/ImageCropPicker.m
+++ ios/src/ImageCropPicker.m
@@ -527,11 +527,18 @@ RCT_EXPORT_METHOD(openCropper:(NSDictionary *)options
     options.networkAccessAllowed = YES;
 
     if ([[[self options] objectForKey:@"multiple"] boolValue]) {
-        NSMutableArray *selections = [[NSMutableArray alloc] init];
+        // NSMutableArray *selections = [[NSMutableArray alloc] init];
+        NSMutableArray *selections = [[NSMutableArray alloc] initWithCapacity: [assets count]];
+
+        for(int i = 0; i<[assets count]; i++) {
+            [selections addObject: [NSNull null]];
+        }
+
 
         [self showActivityIndicator:^(UIActivityIndicatorView *indicatorView, UIView *overlayView) {
             NSLock *lock = [[NSLock alloc] init];
             __block int processed = 0;
+            NSUInteger index = 0;
 
             for (PHAsset *phAsset in assets) {
 
@@ -549,7 +556,7 @@ RCT_EXPORT_METHOD(openCropper:(NSDictionary *)options
                                 return;
                             }
 
-                            [selections addObject:video];
+                            [selections replaceObjectAtIndex: index withObject:video];
                             processed++;
                             [lock unlock];
 
@@ -623,7 +630,7 @@ RCT_EXPORT_METHOD(openCropper:(NSDictionary *)options
                                         exif = [[CIImage imageWithData:imageData] properties];
                                     }
 
-                                    [selections addObject:[self createAttachmentResponse:filePath
+                                    [selections replaceObjectAtIndex: index withObject:[self createAttachmentResponse:filePath
                                                                                 withExif: exif
                                                                            withSourceURL:[sourceURL absoluteString]
                                                                      withLocalIdentifier: phAsset.localIdentifier
@@ -654,6 +661,7 @@ RCT_EXPORT_METHOD(openCropper:(NSDictionary *)options
                             });
                         }];
                     }];
+                        index++;
                 }
             }
         }];
